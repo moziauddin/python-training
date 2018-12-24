2522d4d5a22b475f71de2c9b165af8c13588592b
moziauddin <moziauddin@gmail.com>
2018-12-24 15:10:27 +1100

    Added gitignore, intro files to the repo

---
+++ 01-intro.py
@@ -0,0 +1,24 @@
+print("Hello World")
+print(3)
+print(33)
+print("hello \n world")
+print("hello \t world")
+a = "hello"
+print(a)
+name = 'Peter'
+print(name)
+print(a, name)
+# this is a comment
+''' this is multi line comment
+ends here '''
+print('Patric\'s Pencil')
+print("Patric\'s Pencil")
+print("Patric\"s Pencil")
+# VARIABLES
+a = 1
+num = 2
+num_1 = 3
+num_123 = 4
+print(a)
+a = 5
+print(a)
