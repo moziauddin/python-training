fe8304488000d5840d371a1da46785a3874bea33
moziauddin <moziauddin@gmail.com>
2018-12-24 18:15:57 +1100

    Updated the math operator working file

---
+++ 04-math-operators.py
@@ -0,0 +1,43 @@
+a = 233
+b = 400
+c = 32
+d = 2
+
+# Addition
+print(a + b)
+
+# Subtraction
+print(a - b)
+
+# Simple Division
+print(c/d)
+print(d / c)
+
+# Whole number / Integer division
+print(c // d)
+print(d // c)
+
+# Multiplication
+print(b * d)
+print(b ** d)
+
+# Remainder
+print(a % c)
+
+# BEDMAS
+print(d + (d - c / a) * d + b)
+
+'''
+OUTPUT:
+---------------------------
+633
+-167
+16.0
+0.0625
+16
+0
+800
+160000
+9
+405.725321888412
+'''
