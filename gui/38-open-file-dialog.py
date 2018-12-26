77c7bd57a1df370a0893f05ec189b5ba3f1d8939
moziauddin <moziauddin@gmail.com>
2018-12-26 16:32:32 +1100

    Fixed name formatting

--- gui/38-open-file-dialog.py
+++ gui/38-open-file-dialog.py
@@ -5,12 +5,12 @@ win1 = Tk()
 win1.title('Open File Dialog Example')
 
 
-def openFileDialog():
-    fileLoc = filedialog.askopenfile('r')
-    print("Your pick was:", fileLoc)
-    label1 = Label(text=fileLoc, fg='red').pack()
+def openfiledialog():
+    fileloc = filedialog.askopenfile('r')  # Brings up file open dialogue
+    print("Your pick was:", fileloc)
+    label1 = Label(text=fileloc, fg='red').pack()  # Prints the file name to a label
 
 
-but = Button(text='Open File', width=30, bg='salmon', fg='red', command=openFileDialog)
+but = Button(text='Open File', width=30, bg='salmon', fg='red', command=openfiledialog)
 but.pack()
 win1.mainloop()
