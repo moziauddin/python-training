da69552aab7bea5e06a5ecf6c882c5dc22f765cd
moziauddin <moziauddin@gmail.com>
2018-12-26 16:37:43 +1100

    Added open and read file contents code

--- gui/38-open-file-dialog.py
+++ gui/38-open-file-dialog.py
@@ -9,6 +9,10 @@ def openfiledialog():
     fileloc = filedialog.askopenfile('r')  # Brings up file open dialogue
     print("Your pick was:", fileloc)
     label1 = Label(text=fileloc, fg='red').pack()  # Prints the file name to a label
+    # Open and Read the File Contents
+    filename = fileloc.name
+    fileopen = open(filename)
+    label2 = Label(text=fileopen.read(), fg='blue').pack()
 
 
 but = Button(text='Open File', width=30, bg='salmon', fg='red', command=openfiledialog)
