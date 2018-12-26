17183c87b6d9a3b37aeda36021ec7263e5423eca
moziauddin <moziauddin@gmail.com>
2018-12-26 16:41:07 +1100

    Added Comments

--- gui/38-open-file-dialog.py
+++ gui/38-open-file-dialog.py
@@ -6,12 +6,12 @@ win1.title('Open File Dialog Example')
 
 
 def openfiledialog():
-    fileloc = filedialog.askopenfile('r')  # Brings up file open dialogue
+    fileloc = filedialog.askopenfile('r')  # Brings up file open dialogue. fileloc contains a TextIOWrapper
     print("Your pick was:", fileloc)
     label1 = Label(text=fileloc, fg='red').pack()  # Prints the file name to a label
     # Open and Read the File Contents
-    filename = fileloc.name
-    fileopen = open(filename)
+    filename = fileloc.name  # Gets file name from the TextIOWrapper
+    fileopen = open(filename)  # Opens file as a String/Bytes
     label2 = Label(text=fileopen.read(), fg='blue').pack()
 
 
