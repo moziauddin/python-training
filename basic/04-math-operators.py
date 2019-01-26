89387da34b303e25231d293a1b01741ef6d2c1d1
moziauddin <moziauddin@gmail.com>
2019-01-26 16:24:01 +1100

    Updated math operators

--- basic/04-math-operators.py
+++ basic/04-math-operators.py
@@ -25,10 +25,19 @@ print(b ** d)
 print(a % c)
 
 # BEDMAS
+print(20-10/5*3**2)
 print(d + (d - c / a) * d + b)
 
 # Print all declared variable names...
 print(dir())
+
+# Type checking
+print(type(2))
+print(type(2.0))
+print(type('2'))
+print(type(2.5464617854164651416))
+print(type(24687167186546514684168168716868161461846146848455555555555555555555555555555555555555555555555555))
+
 '''
 OUTPUT:
 ---------------------------
@@ -41,5 +50,12 @@ OUTPUT:
 800
 160000
 9
+2.0
 405.725321888412
+['__annotations__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'a', 'b', 'c', 'd']
+<class 'int'>
+<class 'float'>
+<class 'str'>
+<class 'float'>
+<class 'int'>
 '''
