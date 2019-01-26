d3d6b9d0dcd3a53c91efc3dc097118116856875e
moziauddin <moziauddin@gmail.com>
2019-01-26 22:22:05 +1100

    Updated strings.py

--- basic/17-strings.py
+++ basic/17-strings.py
@@ -32,6 +32,15 @@ print((a + ' ') * 10)  # With spaces
 for i in range(5):
     print('*' * i)
 
+char = 'can you do it?'
+
+# Methods available for a string/integer
+print(char.replace('o', 'i'))
+print(dir(char))
+print(help(char.replace))
+print(char.replace('o', 'i', 1))
+
+
 '''
 Output:
 ----------------------------------------------
@@ -40,6 +49,7 @@ PYTHON Learning
 Lowercase: python
 6
 T
+H
 pyth
 thon
 pythonpythonpythonpythonpythonpythonpythonpythonpythonpython
@@ -49,4 +59,17 @@ python python python python python python python python python python
 **
 ***
 ****
+can yiu di it?
+['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
+Help on built-in function replace:
+
+replace(...) method of builtins.str instance
+    S.replace(old, new[, count]) -> str
+    
+    Return a copy of S with all occurrences of substring
+    old replaced by new.  If the optional argument count is
+    given, only the first count occurrences are replaced.
+
+None
+can yiu do it? 
 '''
